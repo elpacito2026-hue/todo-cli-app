20428451b4d571a7ac314bfb140602c1c260b814
elpacito2026-hue <elpacito2026@gmail.com>
2026-03-25 02:34:05 -0700

    Added README for todo CLI app

---
+++ main.py
@@ -0,0 +1,14 @@
+# To-Do CLI App 📝
+
+A simple command-line application to manage daily tasks.
+
+## Features
+- Add tasks
+- View tasks
+- Delete tasks
+
+## How it works
+You can add your daily tasks, view them anytime, and delete completed ones using simple menu options.
+
+## How to run
+python main.py
